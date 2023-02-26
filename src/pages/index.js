053d901e7a7d0d40dc5e75d6bb1ce442f1aefe1d
agustinmfran <agustinmfran@gmail.com>
2023-02-26 16:51:47 +0100

    layout acomodado

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,14 @@
-import Header from "@/components/Header";
-import InfoSection from "@/pages/InfoSection";
-import AboutSection from "@/pages/AboutSection";
-import ProjectsSection from "@/pages/ProjectsSection";
-import Footer from "@/components/Footer";
-import Head from "next/head";
+import PageLayout from "@/components/PageLayout";
+import InfoSection from "@/components/InfoSection";
+import AboutSection from "@/components/AboutSection";
+import ProjectsSection from "@/components/ProjectsSection";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Agustín Franco</title>
-      </Head>
-      <Header />
-      <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
-        <InfoSection />
-        <AboutSection />
-        <ProjectsSection />
-      </main>
-      <Footer />
-    </>
+    <PageLayout>
+      <InfoSection />
+      <AboutSection />
+      <ProjectsSection />
+    </PageLayout>
   );
 }
