35ba3a3450f9aedeca89409e02f4c65630040445
agustinmfran <agustinmfran@gmail.com>
2023-03-05 10:26:08 +0100

    pulido

--- src/pages/projects/biblo.js
+++ src/pages/projects/biblo.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import es from "../../languages/es";
 import en from "../../languages/en";
 import HeaderProjects from "@/components/HeaderProjects";
@@ -19,9 +20,11 @@ function Biblo() {
       <HeaderProjects />
       <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
         <div className="flex flex-col justify-center items-center pt-40">
-          <h1 className="text-center font-bold text-7xl mb-8 border-b-2 border-teal-500">
-            Biblo{" "}
-          </h1>
+          <Link href="https://github.com/agustinmfran/biblo" target="_blank">
+            <h1 className="text-center font-bold text-7xl mb-8 border-b-2 border-teal-500">
+              Biblo{" "}
+            </h1>
+          </Link>
           <p className="text-center text-2xl">{lang.description}</p>
           <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded" />
           <p className="text-center font-bold text-2xl mb-4">Home Page</p>
