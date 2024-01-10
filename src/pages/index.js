811b7cb26947db64e2b98dedc8ea3216bdfc30d7
Agustin <agustinmfran@gmail.com>
2024-01-10 17:00:55 -0300

    arrow top agregada

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,30 @@ import PageLayout from "@/components/PageLayout";
 import InfoSection from "@/components/InfoSection";
 import AboutSection from "@/components/AboutSection";
 import ProjectsSection from "@/components/ProjectsSection";
+import { HiArrowUp } from "react-icons/hi";
+import { Link } from "react-scroll/modules";
 
 export default function Home() {
   return (
     <PageLayout>
-      <InfoSection />
+      <InfoSection id="home" />
       <AboutSection />
       <ProjectsSection />
+      <div className="flex flex-row justify-center ">
+        <Link
+          className="cursor-pointer"
+          to="home"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+        >
+          <HiArrowUp
+            size={35}
+            className="mt-20 rounded-full border-2 text-neutral-500 border-neutral-500 hover:border-black hover:-translate-y-2 hover:text-black transition-transform cursor-pointer"
+          />
+        </Link>
+      </div>
     </PageLayout>
   );
 }
