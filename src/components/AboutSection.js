a25a2830df108a5c081d404c319033a8d425c474
Agustin <agustinmfran@gmail.com>
2024-02-20 10:55:44 -0300

    pulido

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -45,7 +45,7 @@ const AboutSection = () => {
   const lang = locale === "es" ? es.aboutSection : en.aboutSection;
   return (
     <section id="about">
-      <div className="py-12 pb-12 md:pt-16 md:pb-24">
+      <div className="py-12 pb-12 md:pt-16 md:pb-48">
         <h1 className="text-center font-bold text-4xl">
           {lang.title}{" "}
           <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded" />
